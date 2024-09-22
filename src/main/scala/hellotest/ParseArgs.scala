16038460e25517f290194fb76df4d4a50514e05b
morgan burch <mburch1@luc.edu>
2024-09-22 19:54:40 +0000

    cleaned up code and added comments for more readability

--- src/main/scala/hellotest/ParseArgs.scala
+++ src/main/scala/hellotest/ParseArgs.scala
@@ -10,15 +10,18 @@ object ArgsParser {
     OParser.sequence(
       programName("topwords"),
       head("topwords", "1.0"),
-      opt[Int]('c', "cloud-size")
+
+      opt[Int]('c', "cloud-size")  //arg 1 cloud size
         .valueName("<cloudSize>")
         .action((x, c) => c.copy(cloudSize = x))
         .text("Size of the word cloud (default: 10)"),
-      opt[Int]('l', "length-at-least")
+
+      opt[Int]('l', "length-at-least") //arg 2 minimum length
         .valueName("<lengthAtLeast>")
         .action((x, c) => c.copy(lengthAtLeast = x))
         .text("Minimum length of a word to be considered (default: 6)"),
-      opt[Int]('w', "window-size")
+
+      opt[Int]('w', "window-size") //arg 3 window size
         .valueName("<windowSize>")
         .action((x, c) => c.copy(windowSize = x))
         .text("Size of the moving window (default: 1000)")
