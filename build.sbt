d2461b5141166d8e060a984b3b0c1d69b0467482
Quinn Hasselgren <qhasselgren@luc.edu>
2024-09-25 14:23:10 +0000

    added logging

--- build.sbt
+++ build.sbt
@@ -10,7 +10,9 @@ libraryDependencies ++= Seq(
   "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
   "org.scalacheck" %% "scalacheck" % "1.18.0" % Test, 
   "org.apache.commons" % "commons-collections4" % "4.4", 
-  "com.github.scopt" %% "scopt" % "4.0.1"
+  "com.github.scopt" %% "scopt" % "4.0.1",
+  "org.log4s" %% "log4s" % "1.10.0",
+  "org.slf4j" % "slf4j-simple" % "1.7.30"
 )
 
 enablePlugins(JavaAppPackaging)
--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -3,9 +3,14 @@ import scala.util.matching.Regex
 import scala.collection.mutable
 
 object Main {
+  private val logger = org.log4s.getLogger("Main")
+
   def main(args: Array[String]): Unit = {
     // Validate and parse command-line arguments
     val config = ArgsParser.parseArgs(args).get
+    // Logs parsed arguments
+    logger.info(s"Parsed command-line arguments: $config")
+
     // Regular expression to match non-alphanumeric characters
     val delimiterPattern: Regex = "[^\\p{Alpha}0-9']+".r
 
@@ -19,25 +24,40 @@ object Main {
           .takeWhile(_.isDefined)
           .foreach { lineOpt =>
             lineOpt.foreach { line =>
+              // Logs each line being processed from standard input
+              logger.debug(s"Processing line: '$line'")  
+
               // Split the input line into individual words
               val words = delimiterPattern.split(line).filter(_.nonEmpty)
+              // Logs words after theyre filtered with delimiterPattern
+              logger.debug(s"Words after filtering: ${words.mkString(", ")}")  
+
               words.foreach { word =>
                 if (word.length >= config.lengthAtLeast) {
                   // Add this to recent words because it is long enough to qualify
                   recentWords.enqueue(word)
                   // Add 1 to its count
                   wordCount(word) = wordCount.getOrElse(word, 0) + 1
+                  // Logs an added word
+                  logger.debug(s"Added word: '$word', new count: ${wordCount(word)}")
+
                   // Slide window if it has exceeded the max length
                   if (recentWords.size > config.windowSize) {
                     // Take the oldest word out
                     val oldestWord = recentWords.dequeue()
                     // Subtract 1 from its word count since we removed an instance
                     wordCount(oldestWord) -= 1
+                    // Logs a removed oldest word
+                    logger.debug(s"Removed oldest word: '$oldestWord', new count: ${wordCount.getOrElse(oldestWord, 0)}")
+
                     // Delete it from the map if it no longer appears in the window
                     if (wordCount(oldestWord) <= 0) {
                       wordCount.remove(oldestWord)
+                      // Logs removal from map
+                      logger.debug(s"Removed '$oldestWord' from word count map")
                     }
                   }
+
                   if (recentWords.size >= config.windowSize) {
                     // Sort the map by descending count and limit to the cloud size
                     val sortedWords = wordCount.toSeq
@@ -46,6 +66,8 @@ object Main {
                     // Print in the format suggested by the prompt
                     val output = sortedWords.map { case (w, count) => s"$w: $count" }.mkString(" ")
                     println(output)
+                    // Logs output
+                    logger.info(s"Generated output: $output")
                   }
                 }
               }
